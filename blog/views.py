f37e3b55b6759cd959a305656fe8e35c25c7c524
Manoel Lucio Lustosa Neto <IFRN\20141174010101@cm217646l.ifrn.local>
2017-10-26 10:04:59 -0300

    adicionando views e editando os post do blog.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 def post_list(request):
@@ -9,6 +10,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
